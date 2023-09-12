a9886b518768bf4440666a00f74a04a468dae875
ngoka martins <ngokamartins@hotmail.com>
2023-09-12 16:20:30 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int b;
+	char m;
+
+	for (b = 1 ; b <= 10 ; b++)
+	{
+		for (m = 'a' ; m <= 'z' ; m++)
+			_putchar(m);
+	}
+	_putchar('\n');
+}
