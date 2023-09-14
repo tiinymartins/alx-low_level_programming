0b77bd82381a550e4e8dba84f24f69d66eed3e4c
ngoka martins <ngokamartins@hotmail.com>
2023-09-13 20:20:32 -0700

    task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_times_tables - Entry point
+ *@n: parameter
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int digit, mult, result;
+
+	if (n <= 15 && n >= 0)
+		for (digit = 0; digit <= n; digit++)
+		{
+			_putchar('0');
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				result = digit * mult;
+				if (result <= 99)
+					_putchar(' ');
+				if (result <= 9)
+					_putchar(' ');
+				if (result >= 100)
+				{
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10) % 10) + '0');
+				}
+			}
+		}
+}
+
+
