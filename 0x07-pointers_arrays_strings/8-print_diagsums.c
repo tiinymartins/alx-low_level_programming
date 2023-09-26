22c4bb7c1d6b4c08e447645373e2dde41a055937
ngoka martins <ngokamartins@hotmail.com>
2023-09-25 17:03:30 -0700

    task7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagsums - entry
+ * @a: input
+ * @size: input
+ * Return: 0
+ */
+void print_diagsums(int *a, int size)
+{
+	int num1, num2, h;
+
+	num1 = 0;
+	num2 = 0;
+
+	for (h = 0; h < size; h++)
+	{
+		num1 = num2 + a[h * size + h];
+	}
+	for (h = size - 1; h >= 0; h--)
+	{
+		num2 += a[h * size + (size - h - 1)];
+	}
+	printf("%d, %d\n", num1, num2);
+}
