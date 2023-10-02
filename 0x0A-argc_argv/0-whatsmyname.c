1214eaf8f6a714352f6f26cd5da74e4410e84969
ngoka martins <ngokamartins@hotmail.com>
2023-10-02 12:49:14 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - name of program
+ * @argc: argument
+ * @argv: array
+ * Return: always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
