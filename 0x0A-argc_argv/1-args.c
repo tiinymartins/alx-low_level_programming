3f9f71add70f0b1d6182629d0d504e596a4d0262
ngoka martins <ngokamartins@hotmail.com>
2023-10-02 12:58:35 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - entry
+ * @argc: arguments
+ * @argv: array
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
