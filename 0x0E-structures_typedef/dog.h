56babcbdd20e23966e58a927f62c05b9d2266a0c
ngoka martins <ngokamartins@hotmail.com>
2023-10-10 13:41:28 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
