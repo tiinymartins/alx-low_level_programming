ee438e712fb1f7542bccde7e7f9a49f1500c29f1
ngoka martins <ngokamartins@hotmail.com>
2023-10-19 15:49:42 -0700

    task6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,11 @@
-global	main
+section		.text
 	extern	printf
+	global	main
+
 main:
-	mov  edi, format
-	xor  eax, eax
-	call printf
-	mov  eax, 0
-	ret
-format: db 'Hello, Holberton\n',0
+	mov		edi, msg
+	mov		eax, 0
+	call	printf
+
+section		.data
+	msg db 'Hello, Holberton', 0xa, 0
