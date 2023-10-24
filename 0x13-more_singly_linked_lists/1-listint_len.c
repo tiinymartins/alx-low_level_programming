6347d7d4468fd383efb70ff6ecf4d3dcc708e76d
ngoka martins <ngokamartins@hotmail.com>
2023-10-23 17:49:14 -0700

    task1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 /**
- * listint_int - return the number of elements
+ * listint_len - return the number of elements
  * @h: head of pointer
  * Return: the number of elements
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t num;
+	size_t count;
 
 	if (h == NULL)
 		return (0);
-	for (num = 0; h != NULL; num++)
+	for (count = 0; h != NULL; count++)
 	{
 		h = h->next;
 	}
-	return (num);
+	return (count);
 }
